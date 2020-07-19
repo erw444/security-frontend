caf45bd02b1c3db18c44029b339727b1b8b51d34
Ethan Whittemore <erw444@gmail.com>
2020-07-19 12:52:13 -0500

    Firt commit, with login component

---
+++ src/main/groovy/com/erw/securityfrontend/SecurityFrontendApplication.groovy
@@ -0,0 +1,13 @@
+package com.erw.securityfrontend
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SecurityFrontendApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(SecurityFrontendApplication, args)
+	}
+
+}
---
+++ src/main/resources/static/js/login.component.js
@@ -0,0 +1,6 @@
+angular.module('app').component('login', {
+  templateUrl: 'js/login.html',
+  bindings: {
+    
+  }
+});
